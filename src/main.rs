58bb95a1193417f7c8be7bdf8e4219864a7fc93f
Jacob-JB <jacobburakowski@gmail.com>
2023-09-19 00:18:52 +1000

    done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 
-use wgpu::*;
+use std::time::Instant;
+
+use wgpu::{*, util::{DeviceExt, BufferInitDescriptor}};
 use winit::{event::{Event, WindowEvent, KeyboardInput, ElementState, VirtualKeyCode}, event_loop::ControlFlow};
 
+
+struct View {
+    position: (f32, f32),
+    size: (f32, f32),
+    max_iterations: u32,
+}
+
+impl View {
+    fn compute_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        bytes.append(&mut self.position.0.to_le_bytes().to_vec());
+        bytes.append(&mut self.position.1.to_le_bytes().to_vec());
+
+        bytes.append(&mut self.size.0.to_le_bytes().to_vec());
+        bytes.append(&mut self.size.1.to_le_bytes().to_vec());
+
+        bytes.append(&mut self.max_iterations.to_le_bytes().to_vec());
+
+        // padding
+        bytes.append(&mut vec![0; 4]);
+
+        bytes
+    }
+}
+
+impl Default for View {
+    fn default() -> Self {
+        View {
+            position: (0., 0.),
+            size: (2., 2.),
+            max_iterations: 100,
+        }
+    }
+}
 
 
 
@@ -30,7 +67,7 @@ fn main() {
 
     let (device, queue) = pollster::block_on(adapter.request_device(&DeviceDescriptor {
         label: None,
-        features: Features::empty(),
+        features: Features::BUFFER_BINDING_ARRAY,
         limits: Limits::downlevel_defaults(),
     }, None)).unwrap();
 
@@ -54,25 +91,119 @@ fn main() {
     surface.configure(&device, &surface_config);
 
 
+    let indices = [
+        0, 1, 2, 3, 2, 1,
+    ].into_iter().fold(Vec::new(), |mut acc, i: u16| {
+        for i in i.to_le_bytes() {acc.push(i)}
+        acc
+    });
+
+    let index_buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: Some("Index Buffer"),
+        contents: &indices,
+        usage: BufferUsages::INDEX,
+    });
+
+
+    let mut view = View::default();
+
+    let view_buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: Some("View Buffer"),
+        contents: &view.compute_bytes(),
+        usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+    });
+
+    let view_bind_group_layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+        label: Some("Vew Bind Group Layout"),
+        entries: &[
+            BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::FRAGMENT,
+                ty: BindingType::Buffer {
+                    ty: BufferBindingType::Uniform,
+                    min_binding_size: None,
+                    has_dynamic_offset: false,
+                },
+                count: None,
+            },
+        ],
+    });
+
+    let view_bind_group = device.create_bind_group(&BindGroupDescriptor {
+        label: Some("View Bind Group"),
+        layout: &view_bind_group_layout,
+        entries: &[
+            BindGroupEntry {
+                binding: 0,
+                resource: view_buffer.as_entire_binding(),
+            },
+        ],
+    });
+
+
     let shader_module = device.create_shader_module(ShaderModuleDescriptor {
         label: Some("Shader"),
         source: ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
     });
 
-    let compute_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
-        label: Some("Compute Pipeline Layout"),
-        bind_group_layouts: &[],
+    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+        label: Some("Pipeline Layout"),
+        bind_group_layouts: &[
+            &view_bind_group_layout,
+        ],
         push_constant_ranges: &[],
     });
 
-    let compute_pipeline = device.create_compute_pipeline(&ComputePipelineDescriptor {
-        label: Some("Compute Pipeline"),
-        module: &shader_module,
-        entry_point: "compute_main",
-        layout: Some(&compute_pipeline_layout),
+    let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&pipeline_layout),
+        vertex: VertexState {
+            module: &shader_module,
+            entry_point: "vertex_main",
+            buffers: &[],
+        },
+        fragment: Some(FragmentState {
+            module: &shader_module,
+            entry_point: "fragment_main",
+            targets: &[
+                Some(ColorTargetState {
+                    format: surface_config.format,
+                    blend: Some(BlendState::REPLACE),
+                    write_mask: ColorWrites::all(),
+                })
+            ]
+        }),
+        primitive: PrimitiveState {
+            topology: PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: FrontFace::Ccw,
+            cull_mode: None,
+            unclipped_depth: false,
+            polygon_mode: PolygonMode::Fill,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
     });
 
 
+    let mut up_pressed = false;
+    let mut down_pressed = false;
+    let mut right_pressed = false;
+    let mut left_pressed = false;
+    let mut in_pressed = false;
+    let mut out_pressed = false;
+    let mut increase_pressed = false;
+    let mut decrease_pressed = false;
+
+    let mut fov = 4.;
+
+    let mut last_update = Instant::now();
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent { window_id, ref event } => {
@@ -83,10 +214,29 @@ fn main() {
                 },
 
                 &WindowEvent::Resized(new_size) | &WindowEvent::ScaleFactorChanged { new_inner_size: &mut new_size, .. } => {
-                    surface_config.width = new_size.width;
-                    surface_config.height = new_size.height;
-                    surface.configure(&device, &surface_config);
+                    if new_size.width != 0 && new_size.height != 0 {
+                        surface_config.width = new_size.width;
+                        surface_config.height = new_size.height;
+                        surface.configure(&device, &surface_config);
+                    }
                 },
+
+                WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(key), state, .. }, .. } => 'b: {
+                    *match key {
+                        VirtualKeyCode::W => &mut up_pressed,
+                        VirtualKeyCode::S => &mut down_pressed,
+                        VirtualKeyCode::D => &mut right_pressed,
+                        VirtualKeyCode::A => &mut left_pressed,
+                        VirtualKeyCode::Up => &mut in_pressed,
+                        VirtualKeyCode::Down => &mut out_pressed,
+                        VirtualKeyCode::Right => &mut increase_pressed,
+                        VirtualKeyCode::Left => &mut decrease_pressed,
+                        _ => break 'b,
+                    } = match state {
+                        ElementState::Pressed => true,
+                        ElementState::Released => false,
+                    };
+                }
 
                 _ => (),
             }
@@ -103,6 +253,51 @@ fn main() {
 
             match (|| {
 
+                let delta = last_update.elapsed().as_secs_f32();
+
+                view.position.1 += match (up_pressed, down_pressed) {
+                    (true, false) => 1.,
+                    (false, true) => -1.,
+                    _ => 0.,
+                } * delta * fov * 0.5;
+
+                view.position.0 += match (right_pressed, left_pressed) {
+                    (true, false) => 1.,
+                    (false, true) => -1.,
+                    _ => 0.,
+                } * delta * fov * 0.5;
+
+
+                fov += match (out_pressed, in_pressed) {
+                    (true, false) => 1.,
+                    (false, true) => -1.,
+                    _ => 0.,
+                } * delta * fov * 0.75;
+                fov = fov.min(4.);
+
+                let window_size = window.inner_size();
+                let longest_axis = window_size.width.max(window_size.height) as f32;
+
+                view.size.0 = fov * (window_size.width as f32 / longest_axis);
+                view.size.1 = fov * (window_size.height as f32 / longest_axis);
+
+
+                view.max_iterations = (view.max_iterations as i64 + match (increase_pressed, decrease_pressed) {
+                    (true, false) => 1,
+                    (false, true) => -1,
+                    _ => 0,
+                }).max(0) as u32;
+
+                if increase_pressed || decrease_pressed {
+                    println!("max iterations: {}", view.max_iterations);
+                }
+
+
+                last_update = Instant::now();
+
+                queue.write_buffer(&view_buffer, 0, &view.compute_bytes());
+
+
                 let output_texture = surface.get_current_texture()?;
                 let output_texture_view = output_texture.texture.create_view(&TextureViewDescriptor::default());
 
@@ -110,14 +305,27 @@ fn main() {
                     label: Some("Render Encoder"),
                 });
 
-                let mut compute_pass = encoder.begin_compute_pass(&ComputePassDescriptor {
-                    label: Some("Main Compute Pass"),
+                let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[
+                        Some(RenderPassColorAttachment {
+                            view: &output_texture_view,
+                            resolve_target: None,
+                            ops: Operations {
+                                load: LoadOp::Clear(Color::BLACK),
+                                store: true,
+                            }
+                        })
+                    ],
+                    depth_stencil_attachment: None,
                 });
 
-                compute_pass.set_pipeline(&compute_pipeline);
-                compute_pass.dispatch_workgroups(8, 8, 8);
+                render_pass.set_pipeline(&pipeline);
+                render_pass.set_index_buffer(index_buffer.slice(..), IndexFormat::Uint16);
+                render_pass.set_bind_group(0, &view_bind_group, &[]);
+                render_pass.draw_indexed(0..6, 0, 0..1);
 
-                drop(compute_pass);
+                drop(render_pass);
 
                 queue.submit(std::iter::once(encoder.finish()));
                 output_texture.present();
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,8 +1,67 @@
 
-// compute shader
+struct VertexOutput {
+    @builtin(position) clip_positon: vec4<f32>,
+    @location(0) position: vec2<f32>,
+}
 
 
-@compute_main
-@workgroup_size(1)
-fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+@vertex
+fn vertex_main(
+    @builtin(vertex_index)
+    vertex_index: u32,
+) -> VertexOutput {
+    var out: VertexOutput;
+
+    var x = f32(vertex_index & 1u);
+    var y = f32(vertex_index & 2u) / 2.;
+
+    out.clip_positon = vec4<f32>(
+        x * 2. - 1.,
+        y * 2. - 1.,
+        0.0,
+        1.0,
+    );
+
+    out.position = vec2<f32>(
+        x * 2. - 1.,
+        y * 2. - 1.,
+    );
+
+    return out;
+}
+
+
+struct View {
+    position: vec2<f32>,
+    size: vec2<f32>,
+    max_iterations: u32,
+}
+
+@group(0) @binding(0)
+var<uniform> view: View;
+
+@fragment
+fn fragment_main(
+    vertex_output: VertexOutput
+) -> @location(0) vec4<f32> {
+
+    let x = view.position.x + vertex_output.position.x * view.size.x;
+    let y = view.position.y + vertex_output.position.y * view.size.y;
+
+    var r = 0.;
+    var i = 0.;
+
+    for (var n = 0u; n < view.max_iterations; n ++) {
+        var next_r = r*r - i*i + x;
+        var next_i = 2. * r * i + y;
+
+        r = next_r;
+        i = next_i;
+
+        if (r*r + i*i >= 4.) {
+            return vec4<f32>(f32(n % 20u) / 20., 1., 1., 1.);
+        }
+    }
+
+    return vec4<f32>(0., 0., 0., 1.0);
 }
